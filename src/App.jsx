75b4f5711402a44df253893d33aa1211023b282d
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-05-19 02:38:12 +0530

    feat - added form submit event handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import "./styles.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setTodos(currentTodos => {
+      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false}]
+    });
+    setNewItem("");
+  }
+
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input id="item" value={newItem} onChange={e => setNewItem(e.target.value)} type="text" />
@@ -14,20 +25,19 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {
+          todos.map((item) => {
+            return (
+              <li>
+                <label>
+                  <input type="checkbox"/>
+                  { item.title }
+                </label>
+                <button className="btn btn-danger">Delete</button>
+              </li>
+            );
+          })
+        }
       </ul>
     </>
   )
